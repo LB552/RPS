7e2884789a49a49c5fe5a255c397bd040e31301e
AllanTran90 <allan_sweden90@hotmail.com>
2026-02-11 22:17:23 +0100

    changed place on some code

--- src/websocket.js
+++ src/websocket.js
@@ -1,15 +1,15 @@
 const WebSocket = require("ws");
 const { handleMatchmaking } = require("./matchmaking");
+socket.onopen = () => {
+console.log("✅ WebSocket connected");
+};
+
+socket.onerror = (err) => {
+console.log("❌ WebSocket error", err);
 
 function initWebSocket(server) {
   const wss = new WebSocket.Server({ server });
   
-  socket.onopen = () => {
-  console.log("✅ WebSocket connected");
-};
-
-socket.onerror = (err) => {
-  console.log("❌ WebSocket error", err);
 };
 
   wss.on("connection", (ws) => {
