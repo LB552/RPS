b5c9022c9b0f04964cc011f5b15f6999fbe3a053
AllanTran90 <allan_sweden90@hotmail.com>
2026-02-12 12:41:57 +0100

    debbugging2

--- src/websocket.js
+++ src/websocket.js
@@ -1,16 +1,8 @@
 const WebSocket = require("ws");
 const { handleMatchmaking } = require("./matchmaking");
-socket.onopen = () => {
-console.log("✅ WebSocket connected");
-};
-
-socket.onerror = (err) => {
-console.log("❌ WebSocket error", err);
 
 function initWebSocket(server) {
   const wss = new WebSocket.Server({ server });
-  
-};
 
   wss.on("connection", (ws) => {
     console.log("Player connected");
